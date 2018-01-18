9e3bb55bd9dfc4c4dbcca35263bdf8de03f25d3d
David Chiu <davidwchiu@gmail.com>
2018-01-18 03:19:59 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,14 @@
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0
-    puts "FizzBuzz"
-  elsif int % 3 == 0 # if the number int is divisible by 3
-    puts "Fizz" # Go fizz
-  elsif int % 5 == 0
-    puts "Buzz"
-  elsif int % 3 != 0 && int % 5 != 0
-    puts nil
+   return "FizzBuzz"
   end
+
+  if int % 5 == 0
+   return "Buzz"
+  end
+
+  if int % 3 == 0
+   return "Fizz"
+  end
+
 end

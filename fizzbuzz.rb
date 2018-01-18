fb2de4258cca010b28dc16de1ba20f03eea4cf7b
David Chiu <davidwchiu@gmail.com>
2018-01-18 03:21:05 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,14 +1,11 @@
-def fizzbuzz(int)
-  if int % 3 == 0 && int % 5 == 0
-   return "FizzBuzz"
+def fizzBuzz(number)
+  if number % 3 == 0 && number % 5 == 0
+    "FizzBuzz"
+  elsif number % 3 == 0
+    "Fizz"
+  elsif number % 5 == 0
+    "Buzz"
+  else
+    number
   end
-
-  if int % 5 == 0
-   return "Buzz"
-  end
-
-  if int % 3 == 0
-   return "Fizz"
-  end
-
 end
